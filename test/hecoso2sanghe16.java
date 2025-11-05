9d572e75e8d519aea276ebe2c010b141cf72839b
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-11-05 20:25:29 +0700

    update

--- test/hecoso2sanghe16.java
+++ test/hecoso2sanghe16.java
@@ -3,7 +3,11 @@ package test;
 public class hecoso2sanghe16 {
   public static void main(String[] args) {
     String s = "1101101";
+    // chuyển từ cơ số 2 sang thập phân
     int d2 = Integer.parseInt(s, 2);
     System.out.println(d2);
+    //chuyển từ cơ số thập phân sang cơ số 16
+    String hexString = Integer.toHexString(d2);
+    System.out.println(hexString);
   }
 }
